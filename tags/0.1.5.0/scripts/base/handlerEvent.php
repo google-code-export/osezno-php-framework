a13fc2746923f439fc368ab4ef8ec431d51461b6
osezno.framework <osezno.framework@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2009-03-25 23:16:01 +0000

    Version de lanzamiento

---
+++ tags/0.1.5.0/scripts/base/handlerEvent.php
@@ -0,0 +1,40 @@
+<?php
+  /**
+   * @package OSEZNO PHP FRAMEWORK
+   * @copyright 2007-2009  
+   * @version: 0.1.5
+   * @author: Oscar Eduardo Aldana 
+   * @author: José Ignacio Gutiérrez Guzmán
+   * developer@osezno-framework.com
+   *  
+   * handlerEvent.php: 
+   * Manejador de eventos del Usuario
+   *  
+   */
+
+ include 'dataModel.php';
+ 
+ 
+ 
+/**
+ * Manejador de eventos de usuario
+ *
+ */	
+ class eventos extends myController {
+		
+	
+ }
+
+ 
+ 
+ 
+ /**
+  * No modificar
+  */
+ $objEventos = new eventos($objxAjax);
+ $objOsezno  = new osezno($objxAjax);
+ 
+ $objOsezno->setPathFolderTemplates(PATH_TEMPLATES);
+ $objxAjax->processRequest();
+ 
+?>
