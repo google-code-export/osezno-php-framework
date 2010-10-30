dd7015cbcfd31beabace54d36d16bd91478c3e7d
joselitosinmiedo <joselitosinmiedo@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2010-10-30 16:45:53 +0000

    Avance desarrollo formulario filtro lista. Validacion del formulario, casos de uso filtro.

--- trunk/lib/myDinamicList.class.php
+++ trunk/lib/myDinamicList.class.php
@@ -147,6 +147,12 @@ class myList  {
 	private $resSql;
 	
 	/**
+	 * Numero de registro afectados por la consulta.
+	 * @var integer
+	 */
+	private $numAffectedRows = 0;
+	
+	/**
 	 * Nombre o Id de la lista dinamica
 	 * @var string
 	 */
@@ -564,7 +570,7 @@ class myList  {
 		$getNumFldsAftd = $this->objConn->getNumFieldsAffected();
 		
 		# Numero de registro afectados
-		$getAffectedRows = $this->objConn->getNumRowsAffected();
+		$this->numAffectedRows = $this->objConn->getNumRowsAffected();
 
 		/**
 		 * Calcular el ancho de cada columna si no hay definido
@@ -578,188 +584,194 @@ class myList  {
 		
 		$buf .= '<div id="'.$this->idList.'" name="'.$this->idList.'">'."\n";
 		
-		$buf .=  "\n".'<table border="0" width="'.$this->widthList.''.$this->formatWidthList.'" cellspacing="0" cellpadding="0"><tr><td class="list">'."\n";
-		
 		if (!$this->successFul){
 			
 			$buf .= $this->objConn->getErrorLog(true);
 			
 		}else{
-
-			foreach ($this->arrayWidthsCols as $col => $wid)
-				$totWid -= $wid;
 			
-			$widByCol	= $totWid / ($getNumFldsAftd - count($this->arrayWidthsCols)); 
-		
-			$sw = false;
-		
-			$rows = $this->resSql;
+			if ($this->numAffectedRows){
+				
+				$buf .=  "\n".'<table border="0" width="'.$this->widthList.''.$this->formatWidthList.'" cellspacing="0" cellpadding="0"><tr><td class="list">'."\n";
 			
-			$buf .=  "\n".'<table border="0" width="100%" cellspacing="'.$this->borderCellSize.'" cellpadding="0" id="table_'.$this->idList.'">'."\n";
-
-			$i = 0;
-		
-			$classTd = 'td_default';
-		
-			foreach ($rows as $row){
-
-				if ($this->useDistBetwRows){
-					if ($i%2)
-						$classTd = 'td_default';
-					else	
-						$classTd = 'td_middle';
-				}
+				foreach ($this->arrayWidthsCols as $col => $wid)
+					$totWid -= $wid;
 			
-				/**
-			 	 * Titulos de las columnas
-			 	 */		
-				if (!$sw){
-				
-					$this->arrayFieldsOnQuery = array();
-					
-					$arrColOrd = array ();
-				
-					$sw = true;
-				
-					$bufHead.='<tr>'."\n"."\t";
+				$widByCol	= $totWid / ($getNumFldsAftd - count($this->arrayWidthsCols)); 
+		
+				$sw = false;
+		
+				$rows = $this->resSql;
+			
+				$buf .=  "\n".'<table border="0" width="100%" cellspacing="'.$this->borderCellSize.'" cellpadding="0" id="table_'.$this->idList.'">'."\n";
 
-					$arrayOrdNum = array();
-					
-					$cOrd=1;
-					
-					foreach ($this->arrayOrdNum as $nom){
-						
-						$arrayOrdNum[$nom] = $cOrd;
-						
-						$cOrd++;
-						 
+				$i = 0;
+		
+				$classTd = 'td_default';
+		
+				foreach ($rows as $row){
+
+					if ($this->useDistBetwRows){
+						if ($i%2)
+							$classTd = 'td_default';
+						else	
+							$classTd = 'td_middle';
 					}
+			
+					/**
+			 	 	 * Titulos de las columnas
+			 	 	 */		
+					if (!$sw){
+				
+						$this->arrayFieldsOnQuery = array();
 					
-					foreach ($row as $key => $val){
-					
-						if (!is_numeric($key)){
+						$arrColOrd = array ();
+				
+						$sw = true;
+				
+						$bufHead.='<tr>'."\n"."\t";
 
-							$this->arrayFieldsOnQuery[] = $key;
-							
-							$widCol = 0;
-							if (isset($this->arrayWidthsCols[$key]))
-								$widCol = $this->arrayWidthsCols[$key];
-							else
-								$widCol = $widByCol;
+						$arrayOrdNum = array();
+					
+						$cOrd=1;
+					
+						foreach ($this->arrayOrdNum as $nom){
 						
-							$orderBy = '';	
-
-							$numOrder = '&nbsp;';
-							
-							if (isset($this->arrayOrdMethod[$key])){
-								
-								$orderBy = $this->arrayOrdMethod[$key];
-								
-								$styleName = 'cell_title';
-								
-								if ($orderBy){
-								
-									$cadParam .= '2,';
-								
-									$styleName = 'cell_title_selected';
-								
-									$arrColOrd[] = $key; 
-									
-									$numOrder = $arrayOrdNum[$key];
-									
-								}else{
-									$cadParam .= '1,';
-								}
-							
-								$bufHead.='<td class="'.$styleName.'" width="'.$widCol.'" align="center">';
-							
-								$bufHead.='<table border="0" cellspacing="0" cellpadding="0" width="100%" align="center"><tr><td width="20px" background="'.$this->getSrcImageOrdMethod($orderBy).'" class="num_ord_ref">'.$numOrder.'</td><td width="" style="text-align:center">'; 
-							
-								$bufHead.='<a class="column_title" href="javascript:;" onClick="myListMoveTo(\''.$this->idList.'\',\''.$key.'\')">'.ucwords($this->returnLabelTitle($key)).'</a>';
-
-								$bufHead.='</td><td width="20px">&nbsp;</td></tr></table>';
-							
-								$bufHead.='</td>';
-							
-							}else{
-							
-								$cadParam .= '1,';
-							
-								$bufHead.='<td class="cell_title" width="'.$widCol.'">';
-								
-								$bufHead.='<font class="column_title">'.ucwords($this->returnLabelTitle($key)).'</font>';
-							
-								$bufHead.='</td>';	
-							}
-							
+							$arrayOrdNum[$nom] = $cOrd;
+						
+							$cOrd++;
+						 
 						}
 					
-					}
-				
-					$bufHead .=  '</tr>'."\n";
-				
-					$buf .='{bufHead}';
-			
-				}
-			
-				$buf.='<tr ';
-			
-				$buf.='id="tr_'.$this->idList.'_'.$i.'" ';
-			
-				$buf.='onclick="markRow(this, \''.$classTd.'\',\''.substr($cadParam,0,-1).'\', '.$getNumFldsAftd.')" ';
-			
-				$buf.='onmouseover="onRow(this, '.$getNumFldsAftd.')" ';
-			
-				$buf.='onmouseout="outRow(this, \''.$classTd.'\',\''.substr($cadParam,0,-1).'\', '.$getNumFldsAftd.')" ';
-			
-				$buf.='>'."\n"."\t";
-			
-				foreach ($row as $key => $val){
-				
-					if (!is_numeric($key)){
+						foreach ($row as $key => $val){
 					
-						if (!$val)
-					   		$Value = '&nbsp;';
-					   
-						$buf.='<td class="';
-					
-						if (in_array($key,$arrColOrd))
-							$buf.='cell_content_selected">';
-						else
-							$buf.=''.$classTd.'">';
-						
-						if (isset($this->arrayEventOnColumn[$key])){
-							
-							list($event,$strMsg) = explode('::',$this->arrayEventOnColumn[$key]); 
-							
-							if ($strMsg)
-								$strMsg = ' onClick="return confirm(\''.$strMsg.'\')"';
-							
-							$buf.='<a href="javascript:void('.$event.'(\''.$val.'\',\''.$this->idList.'\'))"'.$strMsg.'>'.ucwords($this->returnLabelTitle($key)).'</a>';
-							
-						}else
-							$buf.=htmlentities($val);	
-					
-						$buf.='</td>';
-					}
-				
-				}
+							if (!is_numeric($key)){
 
-				$buf.= "\n".'</tr>'."\n";
-				$i++;
+								$this->arrayFieldsOnQuery[] = $key;
+							
+								$widCol = 0;
+								
+								if (isset($this->arrayWidthsCols[$key]))
+									$widCol = $this->arrayWidthsCols[$key];
+								else
+									$widCol = $widByCol;
+						
+								$orderBy = '';	
+
+								$numOrder = '&nbsp;';
+							
+								if (isset($this->arrayOrdMethod[$key])){
+								
+									$orderBy = $this->arrayOrdMethod[$key];
+								
+									$styleName = 'cell_title';
+								
+									if ($orderBy){
+								
+										$cadParam .= '2,';
+								
+										$styleName = 'cell_title_selected';
+								
+										$arrColOrd[] = $key; 
+									
+										$numOrder = $arrayOrdNum[$key];
+									
+									}else{
+										$cadParam .= '1,';
+									}
+							
+									$bufHead.='<td class="'.$styleName.'" width="'.$widCol.'" align="center">';
+							
+									$bufHead.='<table border="0" cellspacing="0" cellpadding="0" width="100%" align="center"><tr><td width="20px" background="'.$this->getSrcImageOrdMethod($orderBy).'" class="num_ord_ref">'.$numOrder.'</td><td width="" style="text-align:center">'; 
+							
+									$bufHead.='<a class="column_title" href="javascript:;" onClick="myListMoveTo(\''.$this->idList.'\',\''.$key.'\')">'.ucwords($this->returnLabelTitle($key)).'</a>';
+
+									$bufHead.='</td><td width="20px">&nbsp;</td></tr></table>';
+							
+									$bufHead.='</td>';
+							
+								}else{
+							
+									$cadParam .= '1,';
+							
+									$bufHead.='<td class="cell_title" width="'.$widCol.'">';
+								
+									$bufHead.='<font class="column_title">'.ucwords($this->returnLabelTitle($key)).'</font>';
+							
+									$bufHead.='</td>';	
+								}
+							
+							}
+					
+						}
+				
+						$bufHead .=  '</tr>'."\n";
+				
+						$buf .='{bufHead}';
+			
+					}
+			
+					$buf.='<tr ';
+			
+					$buf.='id="tr_'.$this->idList.'_'.$i.'" ';
+			
+					$buf.='onclick="markRow(this, \''.$classTd.'\',\''.substr($cadParam,0,-1).'\', '.$getNumFldsAftd.')" ';
+			
+					$buf.='onmouseover="onRow(this, '.$getNumFldsAftd.')" ';
+			
+					$buf.='onmouseout="outRow(this, \''.$classTd.'\',\''.substr($cadParam,0,-1).'\', '.$getNumFldsAftd.')" ';
+			
+					$buf.='>'."\n"."\t";
+			
+					foreach ($row as $key => $val){
+				
+						if (!is_numeric($key)){
+					
+							if (!$val)
+					   			$Value = '&nbsp;';
+					   
+							$buf.='<td class="';
+					
+							if (in_array($key,$arrColOrd))
+								$buf.='cell_content_selected">';
+							else
+								$buf.=''.$classTd.'">';
+						
+							if (isset($this->arrayEventOnColumn[$key])){
+							
+								list($event,$strMsg) = explode('::',$this->arrayEventOnColumn[$key]); 
+							
+								if ($strMsg)
+									$strMsg = ' onClick="return confirm(\''.$strMsg.'\')"';
+							
+								$buf.='<a href="javascript:void('.$event.'(\''.$val.'\',\''.$this->idList.'\'))"'.$strMsg.'>'.ucwords($this->returnLabelTitle($key)).'</a>';
+							
+							}else
+								$buf.=htmlentities($val);	
+					
+							$buf.='</td>';
+						}
+				
+					}
+
+					$buf.= "\n".'</tr>'."\n";
+					$i++;
+				}
+			
+				$buf .=  '</tbody>'."\n";
+		
+				$buf .=  '</table>'."\n";
+			
+				$buf .=  '</td></tr></table>'."\n";
+				
+			}else{
+				//TODO: No hay registros afectados
 			}
 			
-			$buf .=  '</tbody>'."\n";
-		
-			$buf .=  '</table>'."\n";
-			
 		}	
-			
-		$buf .=  '</td></tr></table>'."\n";
-
 		
 		# Usar paginacion
-		if ($this->usePagination && $this->successFul){
+		if ($this->usePagination && $this->successFul && $this->numAffectedRows){
 			
 			$arrBut = array(
 				'_ini_page'	 =>array('&nbsp;--&nbsp;','beg','button'),
@@ -804,7 +816,6 @@ class myList  {
 				$objMyForm->addDisabled($this->idList.'_next_page');
 				
 				$objMyForm->addDisabled($this->idList.'_end_page');
-				
 			}
 			
 			if ($this->currentPage==$this->maxNumPage){
@@ -831,9 +842,10 @@ class myList  {
 					
 					case 'field':
 						
-						$objMyForm->addEventJs($this->idList.$id,'onChange','myListPage',array($this->idList,$attr[1]));
+						//$objMyForm->addEventJs($this->idList.$id,'onChange','myListPage',array($this->idList,$attr[1]));
 						
 						$buf .= $objMyForm->getText($this->idList.$id,$attr[0],3,NULL,true);
+						
 					break;
 					
 				}
@@ -896,6 +908,8 @@ class myList  {
 		
 		$objMyForm = new myForm($this->idList.'QueryForm','onSubmitQuery');
 
+		$objMyForm->width = $this->widthList.''.$this->formatWidthList;
+		
 		$objMyForm->border = 0;
 		
 		$objMyForm->selectUseFirstValue = false;
@@ -1019,6 +1033,15 @@ class myList  {
 			}
 		}
 		
+	}
+
+	/**
+	 * Retorna el numero de registros afectados por la anteior consulta.
+	 * @return integer
+	 */
+	public function getNumRowsAffected(){
+		
+		return $this->numAffectedRows;
 	}
 	
 	/**
