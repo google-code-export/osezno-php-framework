40535b616f0d8c3100833b71547f3836f929f98a
joselitosinmiedo <joselitosinmiedo@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2010-12-22 13:23:22 +0000

    Se eliminan tanto archivos temporales como archivos del sistema que afectan el tamaño del paquete del proyecto.

--- application/ejemplo/sql/query.sql
+++
@@ -1,2 +0,0 @@
-select usuario_id as Modificar, nombre as Nombre, apellido as Apellido, edad as Edad, prof.profesion as Profesion from usuarios
-inner join profesiones prof on (usuarios.prof_id = prof.prof_id)
