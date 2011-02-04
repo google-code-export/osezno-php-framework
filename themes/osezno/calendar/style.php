70890a84746e1b0baa7b128242879c0bcf8930f8
joselitosinmiedo <joselitosinmiedo@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2011-02-04 15:26:29 +0000

    Solcuionados varios problemas con el manejo del calendario.

--- themes/osezno/calendar/style.php
+++ themes/osezno/calendar/style.php
@@ -7,10 +7,19 @@
 .cellday {
 	background-color: white;
 	font-family: sans-serif;
-	color: #5F8BBF;
+	color: #000000;
 	text-align: right;
-	height: 22px;
-	width: 22px;
+	height: 28px;
+	width: 28px;
+}
+
+.cellday_over {
+	background-color: #C3D0DF;
+	font-family: sans-serif;
+	color: #000000;
+	text-align: right;
+	height: 28px;
+	width: 28px;
 }
 
 .cellday_today {
@@ -18,8 +27,8 @@
 	font-family: sans-serif;
 	color: black;
 	text-align: right;
-	height: 22px;
-	width: 22px;
+	height: 28px;
+	width: 28px;
 }
 
 .celldays {
@@ -27,8 +36,16 @@
 	font-family: sans-serif;
 	color: white;
 	text-align: center;
-	height: 22px;
-	width: 22px;
+	height: 28px;
+	width: 28px;
+	font-size: small;
+	text-decoration: none;	
+}
+
+.cell_other_days {
+	font-family: sans-serif;
+	color: #BBBBBB;
+	text-align: right;
 	font-size: small;
 	text-decoration: none;	
 }
@@ -37,7 +54,14 @@
 	font-family: sans-serif;
 	font-size: small;
 	text-decoration: none;
-	color: #3F5C7F;	
+	color: #000000;	
+}
+
+.celldays_a:hover {
+	font-family: sans-serif;
+	font-size: small;
+	text-decoration: none;
+	color: #000000;	
 }
 
 .cell_week {
@@ -45,8 +69,8 @@
 	font-family: sans-serif;
 	color: white;
 	text-align: center;
-	height: 22px;
-	width: 22px;
+	height: 28px;
+	width: 28px;
 	font-size: small;	
 }
 
@@ -55,8 +79,8 @@
 	font-family: sans-serif;
 	color: white;
 	text-align: center;
-	height: 22px;
-	width: 22px;
+	height: 28px;
+	width: 28px;
 	font-size: small;	
 }
 
@@ -65,9 +89,6 @@
 }
 
 .calmain {
-	/* background-color: white; */
-	/* border-style: solid; */
-	/* border-color: green; */
-	/* border-width: 1px; */
+	height: auto;
+	width: auto;
 }
-
