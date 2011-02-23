a500341d43508988b4ddbaa0280b936e98fc95e0
joselitosinmiedo <joselitosinmiedo@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2011-02-23 21:42:13 +0000

    Alineacion listas dinamicas

--- trunk/lib/myDinamicList.class.php
+++ trunk/lib/myDinamicList.class.php
@@ -920,7 +920,7 @@ class myList  {
 		
 		$return = $bufHead = $cadParam = '';
 		
-		$buf .= '<div id="'.$this->idList.'" name="'.$this->idList.'">'."\n";
+		$buf .= '<div id="'.$this->idList.'" name="'.$this->idList.'" style="text-align:center;>'."\n";
 		
 		if ($this->useSqlDebug){
 		
