b3003eb7c12e23fbb52e6b368843c07b01a022d3
joselitosinmiedo@gmail.com <joselitosinmiedo@gmail.com@ce0c6192-94c2-11dd-8647-2d95ba4b173f>
2011-09-06 22:41:39 +0000

    Se corrige el issue 20

--- lang/spanish.php
+++ lang/spanish.php
@@ -1,6 +1,13 @@
 <?php
 
 	/**
+	 * Todas las definiciones que usted necesite
+	 */
+	define('OPF_ADD_RECORD','Agregar');
+	define('OPF_SAVE_RECORD','Guardar');
+
+
+	/**
 	 * Exportar datos
 	 */
 	define('MYEXPORT_ERROR_INVALID_FORMAT','El formato a exportar no es valido.');
@@ -49,10 +56,10 @@
 	define('LABEL_HELP_USELIMIT_RULE_FORM','Aplicar limite de registros');
 	define('LABEL_HELP_SELECT_FILEDS_TOSHOW','Seleccione (<b>Ctrl + Click</b>) los campos que desea mostrar en el archivo.');
 	
-	define('MSG_FAILED_MAKE_ACTIVE_TAB','Debe especificar un <a href="http://www.osezno-framework.org/doc/OSEZNO-PHP-FRAMEWORK/myController.html#methodMYTAB_makeActive" target="_blank"><font style="font-size: 14px;">ID de grupo</font></a>.');
-	define('MSG_FAILED_SHOW_FILTER_MUST_PROVIDE_REAL_NAME','Debe especificar al menos un campo con <a href="http://www.osezno-framework.org/doc/OSEZNO-PHP-FRAMEWORK/myList.html#methodsetRealNameInQuery" target="_blank"><font style="font-size: 14px;">alias</font></a>.');
+	define('MSG_FAILED_MAKE_ACTIVE_TAB','Debe especificar un <a href="http://www.osezno-framework.org/doc/OPF/OPF_myController.html#methodMYTAB_makeActive" target="_blank"><font style="font-size: 14px;">ID de grupo</font></a>.');
+	define('MSG_FAILED_SHOW_FILTER_MUST_PROVIDE_REAL_NAME','Debe especificar al menos un campo con <a href="http://www.osezno-framework.org/doc/OPF/OPF_myList.html#methodsetRealNameInQuery" target="_blank"><font style="font-size: 14px;">alias</font></a>.');
 	define('MSG_FAILED_SHOW_FILTER_NO_RECORDS_FOUND','No es posible agregar filtros pues no se encontraron regitros.');
-	define('MSG_FAILED_ORDER_BY_FIELD_MUST_PROVIDE_REAL_NAME','Debe especificar un <a href="http://www.osezno-framework.org/doc/OSEZNO-PHP-FRAMEWORK/myList.html#methodsetRealNameInQuery"><font style="font-size: 14px;">alias</font></a> para este campo.');
+	define('MSG_FAILED_ORDER_BY_FIELD_MUST_PROVIDE_REAL_NAME','Debe especificar un <a href="http://www.osezno-framework.org/doc/OPF/OPF_myList.html#methodsetRealNameInQuery"><font style="font-size: 14px;">alias</font></a> para este campo.');
 	define('MSG_FAILED_SELECT_FIELD_TO_SHOW','Debe seleccionar al menos un campo a mostrar.');
 	define('MSG_SELECT_FIELD_TO_SHOW','Seleccione (<b>Ctrl + Click</b>) los campos que desea mostrar en el archivo.');
 	define('MSG_APPLY_RULES_ALL_VALUES_NULL','Debe escribir al menos un valor.');
